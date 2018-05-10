d02362b2d4ac03ab3eb22ab4cdbe3249c3dd7044
Unknown <edgar.i840@gmail.com>
2018-05-10 15:53:55 +0200

    Added Lava terrain and player dead

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,13 +5,20 @@ public class Player : MonoBehaviour
     [Space]
     public float movementSpeed = 15;
 
-    private GameObject sittingTerrain;
+    [Space]
+    [SerializeField] private Transform checkpoint;
 
+    private GameObject sittingTerrain;
     private Rigidbody myRigidbody;
     private Camera mainCamera;
     private bool jumpAction;
-
     private Vector3 jumpDirection;
+
+    // Dead variables
+    private bool isDead = false;
+    private float deadLerp = 0.7f;
+    private float currentDeadLerp;
+    private Vector3 actualVelocity;
 
 	void Start ()
     {
@@ -21,18 +28,37 @@ public class Player : MonoBehaviour
 	
 	void Update ()
     {
-		if (Input.GetMouseButtonDown(0) && !jumpAction && Cursor.lockState == CursorLockMode.Locked)
+        if (!isDead)
         {
-            jumpDirection = (this.transform.position - mainCamera.transform.position).normalized;
-
-            if (CheckPlatform())
+            if (Input.GetMouseButtonDown(0) && !jumpAction && Cursor.lockState == CursorLockMode.Locked)
             {
-                this.transform.parent = null;
-                myRigidbody.AddForce(jumpDirection * movementSpeed);
-                jumpAction = true;
+                jumpDirection = (this.transform.position - mainCamera.transform.position).normalized;
+
+                if (CheckPlatform())
+                {
+                    this.transform.parent = null;
+                    myRigidbody.AddForce(jumpDirection * movementSpeed);
+                    jumpAction = true;
+                }
             }
         }
-	}
+
+        // Little effect once the player dies
+        else
+        {
+            currentDeadLerp += Time.deltaTime;
+
+            if (currentDeadLerp > deadLerp)
+                currentDeadLerp = deadLerp;
+
+            float perc = currentDeadLerp / deadLerp;
+
+            myRigidbody.velocity = Vector3.Lerp(actualVelocity, Vector3.zero, perc);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            Restart();
+    }
 
     // Check if the direction the player is trying to jump in does not belong to the same terrain he is on, to avoid bugs
     private bool CheckPlatform()
@@ -46,12 +72,33 @@ public class Player : MonoBehaviour
         return true;
     }
 
+    private void Restart()
+    {
+        isDead = false;
+        jumpAction = false;
+
+        this.transform.position = checkpoint.transform.position;
+    }
+
     void OnCollisionEnter(Collision collision)
     {
-        sittingTerrain = collision.gameObject;          // Set the new terrain where the player is sitting on
-        jumpAction = false;                             // Let the player jump again
-        myRigidbody.velocity = Vector3.zero;            // Velocity = 0 so it stops on collision
+        if (!collision.gameObject.tag.Equals("Lava"))
+        {
+            sittingTerrain = collision.gameObject;          // Set the new terrain where the player is sitting on
+            jumpAction = false;                             // Let the player jump again
+            myRigidbody.velocity = Vector3.zero;            // Velocity = 0 so it stops on collision
 
-        this.transform.parent = collision.transform;
+            this.transform.parent = collision.transform;
+        }
+
+        else
+        {
+
+            myRigidbody.velocity *= -1;
+            currentDeadLerp = 0f;
+            actualVelocity = myRigidbody.velocity;
+
+            isDead = true;
+        } 
     }
 }
